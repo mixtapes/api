88807d76596d261330686bb0e8dbf39cecffbcf6
Attila Györffy <attila@attilagyorffy.com>
2015-01-18 12:37:15 +0100

    Break down code for readability

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,13 @@
 class Profile
   include Mongoid::Document
+
   field :name, type: String
   field :permalink, type: String
 
   # Associations
   embeds_many :tracklists
 
+  # Validations
   validates :name, presence: true
   validates :permalink, presence: true
 
